7a4ada925e1fd1a011b80d9cfd0c5197a1d424e6
Moein Alizade <m.alizade58010@gmail.com>
2021-03-20 00:12:34 +0330

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/**/*.js',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        '200': '50rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
